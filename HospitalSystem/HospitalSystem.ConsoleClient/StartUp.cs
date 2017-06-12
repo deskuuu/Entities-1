a7c8cffac26d4e8946897937b431923229991df2
desi <desislava.manova@gmail.com>
2017-06-12 12:43:14 +0300

    Fix connection string

--- HospitalSystem/HospitalSystem.ConsoleClient/StartUp.cs
+++ HospitalSystem/HospitalSystem.ConsoleClient/StartUp.cs
@@ -5,16 +5,34 @@
 
     using HospitalSystem.Data;
     using HospitalSystem.Data.Migrations;
+    using HospitalSystem.Models;
+    using System.Linq;
 
     public class StartUp
     {
         public static void Main()
         {
-            Database.SetInitializer(new MigrateDatabaseToLatestVersion<HospitalSystemDbContext, Configuration>());
+            //Database.SetInitializer(new MigrateDatabaseToLatestVersion<HospitalSystemDbContext, Configuration>());
 
             // Importer.Create(Console.Out).Import();
 
             // DataSearcher.Search(new HospitalSystemService());
+
+            try
+            {
+                var db = new HospitalSystemDbContext();
+
+                var pat = new Patient() { FirstName = "anna", LastName = "eliii", Age = 5 };
+
+                db.Patients.Add(pat);
+                db.SaveChanges();
+
+                Console.WriteLine(db.Patients.Count());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("here");
+            }
         }
     }
 }
